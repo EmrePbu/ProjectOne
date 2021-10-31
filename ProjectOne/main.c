14bb65ce4eb84afa68d6de589b5d2e98151246fe
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2021-10-31 22:03:30 +0300

    Adding sha256 algorithm

--- ProjectOne/main.c
+++ ProjectOne/main.c
@@ -1,7 +1,64 @@
 ﻿#define _DEFAULT_SOURCE
 
 #include <stdio.h>
+#include <assert.h>
 #include "recdir.h"
+#include "sha256.h"
+
+char hex_digit(unsigned int digit)
+{
+	digit %= 0x10;
+	if (digit <= 9)
+	{
+		return digit + '0';
+	}
+	if (10 <= digit && digit <= 15)
+	{
+		return digit + 'a';
+	}
+	assert(0 && "unreachable");
+}
+
+void hash_as_cstr(BYTE hash[32], char output[32 * 2 + 1])
+{
+	for (size_t i = 0; i < sizeof(hash); i++)
+	{
+		output[i * 2 + 0] = hex_digit(hash[i] / 0x10);
+		output[i * 2 + 1] = hex_digit(hash[i] % 0x10);
+	}
+	output[32 * 2] = '\0';
+}
+
+void hash_of_file(const char* file_path, BYTE hash[32])
+{
+	SHA256_CTX ctx;
+	memset(&ctx, 0, sizeof(ctx));
+	sha256_init(&ctx);
+
+	FILE* f = fopen(file_path, "rb");
+	if (f == NULL)
+	{
+		fprintf(stderr, "Could not open file %s: %s\n", file_path, strerror(errno));
+		exit(1);
+	}
+
+	BYTE buffer[1024];
+	size_t buffer_size = fread(buffer, sizeof(buffer), 1, f);
+	while (buffer_size > 0)
+	{
+		sha256_update(&ctx, buffer, buffer_size);
+		buffer_size = fread(buffer, sizeof(buffer), 1, f);
+	}
+
+	if (ferror(f))
+	{
+		fprintf(stderr, "Could not read from file %s: %s\n", file_path, strerror(errno));
+		exit(1);
+	}
+	fclose(f);
+
+	sha256_final(&ctx, hash);
+}
 
 
 int main(int argc, char const** argv)
