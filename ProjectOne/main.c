85dc9c5d889f71122de6ed608483f3844c3817ce
Emre Pbu <33737009+EmrePbu@users.noreply.github.com>
2021-11-07 14:28:41 +0300

    Comparing hash of file.

--- ProjectOne/main.c
+++ ProjectOne/main.c
@@ -4,6 +4,13 @@
 #include <assert.h>
 #include "recdir.h"
 #include "sha256.h"
+#define STB_DS_IMPLEMENTATION
+#include "stb_ds.h"
+
+typedef struct
+{
+	BYTE bytes[32];
+}Hash;
 
 char hex_digit(unsigned int digit)
 {
@@ -19,17 +26,17 @@ char hex_digit(unsigned int digit)
 	assert(0 && "unreachable");
 }
 
-void hash_as_cstr(BYTE hash[32], char output[32 * 2 + 1])
+void hash_as_cstr(Hash hash, char output[32 * 2 + 1])
 {
 	for (size_t i = 0; i < 32; i++)
 	{
-		output[i * 2 + 0] = hex_digit(hash[i] / 0x10);
-		output[i * 2 + 1] = hex_digit(hash[i] % 0x10);
+		output[i * 2 + 0] = hex_digit(hash.bytes[i] / 0x10);
+		output[i * 2 + 1] = hex_digit(hash.bytes[i] % 0x10);
 	}
 	output[32 * 2] = '\0';
 }
 
-void hash_of_file(const char* file_path, BYTE hash[32])
+void hash_of_file(const char* file_path, Hash* hash)
 {
 	SHA256_CTX ctx;
 	memset(&ctx, 0, sizeof(ctx));
@@ -43,7 +50,7 @@ void hash_of_file(const char* file_path, BYTE hash[32])
 	}
 
 	BYTE buffer[1024];
-	size_t buffer_size = fread(buffer, 1, sizeof(buffer),  f);
+	size_t buffer_size = fread(buffer, 1, sizeof(buffer), f);
 	while (buffer_size > 0)
 	{
 		sha256_update(&ctx, buffer, buffer_size);
@@ -57,35 +64,45 @@ void hash_of_file(const char* file_path, BYTE hash[32])
 	}
 	fclose(f);
 
-	sha256_final(&ctx, hash);
+	sha256_final(&ctx, hash->bytes);
 }
+
+typedef struct
+{
+	Hash key;
+	char** paths;
+}Record;
+
+Record* db = NULL;
 
 int main(int argc, char const** argv)
 {
 	RECDIR* recdir = recdir_open("C:\\Users\\emrep\\Desktop\\a");
 	errno = 0;
 	struct dirent* ent = recdir_read(recdir);
-	//---save file---
-	FILE* f = fopen("C:\\Users\\emrep\\Desktop\\a\\file.txt", "w");
-	if (f == NULL)
-	{
-		printf("Error opening file!\n");
-		exit(1);
-	}
-	//---end save file---
+
 	while (ent)
 	{
-		BYTE hash[32];
-		char hash_cstr[32 * 2 + 1];
+		Hash hash;
 		char* path = join_path(recdir_top(recdir)->path, ent->d_name);
-		hash_of_file(path, hash);
-		hash_as_cstr(hash, hash_cstr);
-		//printf("recdir file: %s => %s\n", path, hash_cstr);
-		fprintf(f,"%s => %s\n",  hash_cstr, path); //ProjectOne\out\build\x64-Debug\ProjectOne\file.txt
+		hash_of_file(path, &hash);
 
-		free(path);
+		ptrdiff_t index = hmgeti(db, hash);
+		if (index < 0)
+		{
+			Record record;
+			record.key = hash;
+			record.paths = NULL;
+			arrput(record.paths, path);
+			hmputs(db, record);
+		}
+		else
+		{
+			arrput(db[index].paths, path);
+		}
 
 		ent = recdir_read(recdir);
+		//free(path);
 	}
 
 	if (errno != 0)
@@ -93,10 +110,22 @@ int main(int argc, char const** argv)
 		fprintf(stderr, "ERROR: could not read the director: %s\n", recdir_top(recdir)->path);
 		exit(1);
 	}
-	//---save file---
-	fclose(f);
-	//---end save file---
 	recdir_close(recdir);
+
+	for (ptrdiff_t i = 0; i < hmlen(db); ++i)
+	{
+		if (arrlen(db[i].paths) > 1) 
+		{
+			char output[32 * 2 + 1];
+			hash_as_cstr(db[i].key, output);
+			printf("%s\n", output);
+
+			for (ptrdiff_t j = 0; j < arrlen(db[i].paths); ++j)
+			{
+				printf("    %s\n", db[i].paths[j]);
+			}
+		}
+	}
 
 	return 0;
 }
@@ -109,7 +138,7 @@ int main(int argc, char const** argv)
 }
 
 cmd > *.exe emre
-out > emre 
+out > emre
 
 cmd > *.exe 123
 out > 123
